b827657481d8961f49aa9c6aa596c62be55367aa
root <root@localhost.localdomain>
2017-12-09 20:37:22 -0500

    removed unnecessary variables

--- db/index.js
+++ db/index.js
@@ -1,6 +1,16 @@
-module.exports.getIPs=(ips)=>{
-    return ips.chain()
+const loki = require('lokijs');
+const db = new loki('ipinfo');
+var ips = db.addCollection('ipinfo', {indices: ['ip']})
+module.exports.ips = ips
+//module.exports = ips;
+
+module.exports.getIPs=()=>{
+    return this.ips.chain()
     .where(function(obj){ return obj.ip.indexOf('.') != -1})
     .simplesort('ip')
     .data()
+}
+
+module.exports.getIP=(ip)=>{
+    return this.ips.find({'ip':{'$eq': ip}})
 }

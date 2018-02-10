9b1f4bac0890db36d7182ef1da6871f66a9dc0fd
root <root@localhost.localdomain>
2018-02-10 13:05:58 -0500

    Starting to implement actual useful logging, as well as using broadcasting to all clients instead of just some.  Soon I will broadcast changes only unless it's an initial connection

--- discover/index.js
+++ discover/index.js
@@ -1,7 +1,7 @@
 const util = require('../util');
 const db = require('../db');
 const ips = require('../db').ips
-const {spawn} = require('child_process');
+const spawn = require('child_process').spawn;
 
 isScanning = false;
 
@@ -48,13 +48,13 @@ module.exports.discoverService = (host, port, fn)=>{
     })
 }
 
-module.exports.scan = (ip, range, ports, ws)=>{
+module.exports.scan = (ip, range, ports, broadcast)=>{
     isScanning = true
     ip = ip || '173.62.7.72'
     range =  range || '24'
     ports = ports || '7,9,13,21,22,23,25,26,37,53,79-81,88,106,110,111,113,119,135,139,143-144,179,199,389,427,443,444,445,465,513,514,515,543,544,548,587,631,646,873,990,993,995,1025,1029,1110,1433,1720,1723,1755,1900,2000,2001,2049,2121,2717,3000,3128,3306,3389,3986,4899,5000,5009,5051,5060,5101,5190,5357,5432,5631,5666,5800,5900,6000,6001,6646,8000,8008,8009,8080,8081,8443,8888,9100,9999,10000,32768,49152,49153,49154,49155,49156,49157'
     const scanner = spawn('masscan', [(ip + '/' + range),'--rate=400','-p', ports]);
-    ws.send(JSON.stringify({type: 'scanStart', data: db.getIPs()}))
+    broadcast(JSON.stringify({type: 'scanStart', data: db.getIPs()}))
     scanner.stdout.on('data', (data)=>{
         var port = util.stripNums(data.toString().slice(21,25))
         var ip = util.stripNums(data.toString().slice(31,48))
@@ -75,11 +75,11 @@ module.exports.scan = (ip, range, ports, ws)=>{
                 record[0][port] = service
                 ips.update(record)
                   
-                ws.send(JSON.stringify({type: 'ipdata', data: db.getIPs()}))
+                broadcast(JSON.stringify({type: 'ipdata', data: db.getIPs()}))
             }else{
                 newRow[port] = service
                 ips.update(newRow)
-                ws.send(JSON.stringify({type: 'ipdata', data: db.getIPs()}))
+                broadcast(JSON.stringify({type: 'ipdata', data: db.getIPs()}))
                 
             }
         })
@@ -87,7 +87,7 @@ module.exports.scan = (ip, range, ports, ws)=>{
         console.log(ip + ':' + port)
         
 
-        ws.send(JSON.stringify({type: 'ipdata', data: db.getIPs()}))
+        broadcast(JSON.stringify({type: 'ipdata', data: db.getIPs()}))
     })
     
     scanner.stderr.on('data', (data)=>{
@@ -96,7 +96,7 @@ module.exports.scan = (ip, range, ports, ws)=>{
     
     scanner.on('close', (code)=>{
         isScanning = false
-        ws.send(JSON.stringify({
+        broadcast(JSON.stringify({
             type: 'ipdata', 
             data: db.getIPs(),
          complete: true
--- ws/index.js
+++ ws/index.js
@@ -1,27 +1,26 @@
 const WebSocket = require('ws');
 const discover = require('../discover');
+const inspect = require('util').inspect
 const db = require('../db');
-
-
+const log = require('../log')
 const wss = new WebSocket.Server({ port: 3000 });
-console.log('server is listening on 3000!')
-// When a connection occcurs, start scanning!
-function heartbeat() {
-    this.isAlive = true;
+    log.info('Server started on port 3000')
+    // When a connection occcurs, start scanning!
+    function heartbeat() {
+        this.isAlive = true;
 }
 
-wss.on('connection', function connection(ws, req){
-    console.log('Someone Connected!')
+wss.on('connection', function connection(ws){
+    log.info('Websocket connection initiated')
     ws.isAlive = true;
     ws.on('pong', heartbeat)
-    ws.send(JSON.stringify({
-        type: 'signal', 
+    broadcast(JSON.stringify({
+        type: 'connection',
         message: 'connected', 
         data: db.getIPs()
     }))
-    discover.scan('69.206.112.85','16',null,ws)
+    discover.scan('69.206.112.85','16',null,broadcast)
 })
-
 const broadcast = (data)=>{
         wss.clients.forEach((client)=>{
           if (client.readyState === WebSocket.OPEN) {
